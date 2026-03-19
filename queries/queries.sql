8fc737c6037a970210cdda52fccc456c7bcd5565
aproposito <aproposito@gmail.com>
2026-03-19 19:09:35 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -188,7 +188,7 @@ FROM departamento d
 LEFT JOIN profesor p
 	ON d.id = p.id_departamento
 GROUP BY d.nombre
-ORDER BY total DESC;
+ORDER BY total DESC, d.nombre;
 
 -- 20. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. Tingues en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d'aparèixer en el llistat. El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures. (grau, total)
 SELECT 
